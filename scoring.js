8988b72a74a674d9e0ed6b25b10dd52dc12d3ca6
yusuke kuramochi <kuramochuwo@gmail.com>
2022-07-19 00:57:56 +0000

    課題提出

--- scoring.js
+++ scoring.js
@@ -35,20 +35,51 @@ $(document).ready(function () {
       return "A";
     }
     // もし「averageIndicate」が60以上なら"B"を返します。
-    else if (averageIndicate >= 60) {
-      return "B";
-    }
-    // もし「averageIndicate」が40以上なら"C"を返します。
-    else if (averageIndicate >= 40) {
-      return "C";
-    }
-    // もし「averageIndicate」がそれ以外なら"D"を返します。
-    else {
-      return "D";
-    }
+  else if (averageIndicate >= 60) {
+    return "B";
+  }
+  // もし「averageIndicate」が40以上なら"C"を返します。
+  else if (averageIndicate >= 40) {
+    return "C";
+  }
+  // もし「averageIndicate」がそれ以外なら"D"を返します。
+  else {
+    return "D";
+  }
+
+  };
+  // 各教科の点数を取得し、取得した点数から「合格、不合格」の判断を下すロジックを作ります。
+  function get_pass_or_failure() {
+    let subject_points = [Number($('#national_language').val()),
+    Number($('#english').val()),
+    Number($('#mathematics').val()),
+    Number($('#science').val()),
+    Number($('#society').val())
+    ];
+    // 変数「number」に入力した教科の数を代入します。
+    let number = subject_points.length;
+    // 変数「judge」に"合格"を代入しておきます。
+    let judge = "合格";
+    for(let i=0; i<number; i++){
+   if(subject_points[i]<60){
+     judge= "不合格";
+     break;
+   }
+ }
+    // 入力したそれぞれの教科のうち、1つでも60点よりも低い点数があった場合、変数「judge」に"不合格"を再代入する処理を記述する。
+    // ヒント：配列の繰り返し処理について調べてみましょう。
+    return judge;
   };
 
-
+  // 最終的なジャッジのロジックを作ります。
+  function judgement() {
+    // 変数「achievement」に「get_achievement()の戻り値」を代入します。
+    let achievement = get_achievement();
+    // 変数「pass_or_failure」に「get_pass_or_failure()の戻り値」を代入します。
+    let pass_or_failure = get_pass_or_failure();
+    // 「最終ジャッジ」(id="alert-indicate)ボタンを押したら「あなたの成績は${achievement}です。${pass_or_failure}です。」が出力される処理です。
+    $('#declaration').append(`<label id="alert-indicate" class="alert alert-info">あなたの成績は${achievement}です。${pass_or_failure}です。</label>`);
+  };
   // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]のいずれかの点数が変更された際に「function score_indicate()」を発火させる処理です。
   $('#national_language, #english, #mathematics, #science, #society').change(function () {
     score_indicate();
@@ -57,5 +88,16 @@ $(document).ready(function () {
   $('#btn-evaluation').click(function () {
     $("#evaluation").text(get_achievement());
   });
+  // 「判定」(id="btn-judge")ボタンを押したら「function et_pass_or_failure()」が出力される処理です。
+  $('#btn-judge').click(function () {
+    $("#judge").text(get_pass_or_failure());
+  });
+  // 「最終ジャッジ」(id="btn-declaration")ボタンが押された際、「function judgement()」の処理を実行させる。
+  // ２回目以降に「最終ジャッジ」ボタンを押した際は、それまでに表示していたジャッジのHTML要素を削除して、新たなジャッジのHTML要素を追加する。
+  // ヒント：removeメソッドについて調べてみましょう。
+  $('#btn-declaration').click(function () {
+    $("#declaration").text("");
+    $("#declaration").text(judgement());
 
+  });
 });
